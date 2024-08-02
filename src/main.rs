9216389cf4cabd2d9ca023e194507027a795e2ae
watasuke102 <watasuke102@gmail.com>
2024-08-02 20:47:11 +0900

    update: add whitespace()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
 }
 
+fn whitespace(mut input: &str) -> &str {
+  loop {
+    let mut chars = input.chars();
+    if chars.next() == Some(' ') {
+      input = chars.as_str();
+    } else {
+      break;
+    }
+  }
+  input
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
+
+  #[test]
+  fn test_whitespace() {
+    assert_eq!(whitespace("    "), "");
+  }
 }

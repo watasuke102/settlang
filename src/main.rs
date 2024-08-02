d6656047289a977a21d6744d1d7fa58ff1b304fa
watasuke102 <watasuke102@gmail.com>
2024-08-02 21:08:22 +0900

    update: add expect_str()

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,28 @@ fn whitespace(mut input: &str) -> &str {
   input
 }
 
+/// If input begins with test, consume it and return (true, <consumed input>)
+/// otherwise return (false, <non-consumed input>)
+fn expect_str<'s>(input: &'s str, test: &'s str) -> (bool, &'s str) {
+  assert_ne!(test.len(), 0);
+  let mut input_chars = input.chars();
+  let mut test_chars = test.chars();
+  loop {
+    // End of test; means every char is matched
+    let Some(test_next) = test_chars.next() else {
+      return (true, input_chars.as_str());
+    };
+    // end of input
+    let Some(input_next) = input_chars.next() else {
+      break;
+    };
+    if input_next != test_next {
+      break;
+    }
+  }
+  return (false, input);
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -20,5 +42,29 @@ mod test {
   #[test]
   fn test_whitespace() {
     assert_eq!(whitespace("    "), "");
+  }
+  #[test]
+  fn expect_str_is_true_when_same_input() {
+    let input = "fn";
+    let test = "fn";
+    let (is_matched, returned_input) = expect_str(input, test);
+    assert!(is_matched);
+    assert_eq!(returned_input, "");
+  }
+  #[test]
+  fn expect_str_is_false_if_different_input() {
+    let input = "test";
+    let test = "fn";
+    let (is_matched, returned_input) = expect_str(input, test);
+    assert!(!is_matched);
+    assert_eq!(returned_input, input);
+  }
+  #[test]
+  fn expect_str_is_false_when_same_input_with_pre_spaces() {
+    let input = "     fn";
+    let test = "fn";
+    let (is_matched, returned_input) = expect_str(input, test);
+    assert!(!is_matched);
+    assert_eq!(returned_input, input);
   }
 }

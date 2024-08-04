7fe37fb7fa38fb4422295bd69fbb2d1b746aa4f7
watasuke102 <watasuke102@gmail.com>
2024-08-04 19:19:58 +0900

    update: treat return as a statement

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -8,6 +8,7 @@ pub enum Expression {
 #[derive(Debug)]
 pub enum Statement {
   DeclStatement(Declaration),
+  Return(Expression),
 }
 
 #[derive(Debug)]
@@ -25,19 +26,36 @@ pub fn expect_code(mut input: &str) -> ParseResult<Vec<Statement>> {
 }
 
 fn expect_statement(input: &str) -> ParseResult<Statement> {
-  let res = expect_declaration(input)?;
-  Ok((Statement::DeclStatement(res.0), res.1))
+  let res = expect_return(input);
+  if res.is_ok() {
+    let res = res.unwrap();
+    return Ok((Statement::Return(res.0), res.1));
+  }
+
+  let res = expect_declaration(input);
+  if res.is_ok() {
+    let res = res.unwrap();
+    return Ok((Statement::DeclStatement(res.0), res.1));
+  }
+  Err(error::ParseError::ExpectedStatement)
 }
 fn expect_declaration(input: &str) -> ParseResult<Declaration> {
   let res = expect_fn_declaration(input)?;
   Ok((Declaration::FnDecl(res.0), res.1))
 }
 
+fn expect_return(input: &str) -> ParseResult<Expression> {
+  let Ok((_, input)) = expect_str(mulspace_0(input), "return") else {
+    return Err(error::ParseError::UnexpectedStr);
+  };
+  expect_expression(mulspace_1(input)?)
+}
+
 #[derive(Debug)]
 pub struct Function {
-  name:   String,
+  name: String,
   // TODO: arguments
-  retval: Expression, // Expression
+  code: Vec<Statement>,
 }
 fn expect_fn_declaration(mut input: &str) -> Result<(Function, &str), error::ParseError> {
   input = expect_str(mulspace_0(input), "fn")?.1;
@@ -47,13 +65,12 @@ fn expect_fn_declaration(mut input: &str) -> Result<(Function, &str), error::Par
   // TODO: arguments
   input = expect_char(mulspace_0(input), ')')?.1;
   input = expect_char(mulspace_0(input), '{')?.1;
-  input = expect_str(mulspace_0(input), "return")?.1;
 
-  let retval;
-  (retval, input) = expect_expression(mulspace_1(input)?)?;
+  let code;
+  (code, input) = expect_code(input)?;
 
   input = expect_char(mulspace_0(input), '}')?.1;
-  Ok((Function { name, retval }, input))
+  Ok((Function { name, code }, input))
 }
 
 /// TODO: this accepts only number
@@ -109,7 +126,38 @@ mod test {
         }
       };
       assert_eq!(res.0.name, fname.to_string());
-      assert_eq!(res.0.retval, Expression::Constant(retval));
+      assert_eq!(res.0.code.len(), 1);
+      let Statement::Return(ref expr) = res.0.code[0] else {
+        panic!(
+          "Statement is wrong; expect: Return, actual: {:?}",
+          res.0.code[0]
+        );
+      };
+      let Expression::Constant(parsed_retval) = expr else {
+        panic!("Expression is wrong; expect: Constant, actual: {:?}", expr);
+      };
+      assert_eq!(*parsed_retval, retval);
+    }
+  }
+
+  #[test]
+  fn test_expect_return() {
+    for (input, is_expected_success, retval) in [
+      ("return 0", true, 0),
+      ("   return 128", true, 128),
+      ("return0", false, 0),
+    ] {
+      let Ok(res) = expect_return(input) else {
+        if is_expected_success {
+          panic!("input `{}` is expected to succeed but it fails", input);
+        } else {
+          continue;
+        }
+      };
+      let Expression::Constant(parsed_retval) = res.0 else {
+        panic!("Expression is wrong; expect: Constant, actual: {:?}", res.0);
+      };
+      assert_eq!(parsed_retval, retval);
     }
   }
   #[test]

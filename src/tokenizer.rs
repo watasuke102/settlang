ce9ad0d1b1dfcdd6cdfbb055527917bfdb011d70
watasuke102 <watasuke102@gmail.com>
2024-08-04 11:04:38 +0900

    update: add expect_expression()

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,6 +3,22 @@ use crate::{error, parser::*};
 #[derive(Debug, PartialEq)]
 pub enum Token {
   Identifier(String),
+  Expression(i32), // TODO: it should be token tree
+}
+
+/// TODO: this accepts only number
+fn expect_expression(mut input: &str) -> Result<(Token, &str), error::ParseError> {
+  let res = num_1(input)?;
+  let mut constant = res.0;
+  input = res.1;
+  loop {
+    let res = num_0(input);
+    if res.0.is_none() {
+      return Ok((Token::Expression(constant), input));
+    }
+    constant = constant * 10 + res.0.unwrap();
+    input = res.1;
+  }
 }
 
 /// consume Identifier and return (it, consumed input)
@@ -25,6 +41,27 @@ fn expect_identifier(mut input: &str) -> Result<(Token, &str), error::ParseError
 mod test {
   use super::*;
 
+  #[test]
+  fn expect_expression_returns_expression_if_input_begins_with_number() {
+    let input = "123 abc";
+    let res = expect_expression(input);
+    assert!(res.is_ok());
+    let res = res.unwrap();
+    let Token::Expression(constant) = res.0 else {
+      panic!(
+        "expect_expression({}) succeeded but does not return Token::Expression (returned: {:?})",
+        input, res.0
+      );
+    };
+    assert_eq!(constant, 123);
+    assert_eq!(res.1, " abc");
+  }
+  #[test]
+  fn expect_expression_fails_if_input_begins_with_alphabet() {
+    let input = "abc 123";
+    let res = expect_expression(input);
+    assert!(res.is_err());
+  }
   #[test]
   fn expect_identifier_returns_str_before_whitespace_and_consumed_input() {
     let input = "name01  name02";

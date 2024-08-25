6a5088128c063380493771f6edc616ee5972bc69
watasuke102 <watasuke102@gmail.com>
2024-08-26 00:06:31 +0900

    update: implement simple CLI compiler

--- cli_compiler/src/main.rs
+++ cli_compiler/src/main.rs
@@ -1,197 +1,99 @@
-use std::io::Write;
+use std::{env, fs, io::Write, process};
+
+struct BuildOptions {
+  output_path: Option<String>,
+  input_path:  Option<String>,
+  verbose:     bool,
+}
+impl BuildOptions {
+  fn new() -> Self {
+    BuildOptions {
+      output_path: None,
+      input_path:  None,
+      verbose:     false,
+    }
+  }
+}
 
 fn main() {
-  for (i, code) in [
-    // errors
-    r"
-fn error0( -> i32 {}
-",
-    r"
-fn error1() 1+1
-",
-    r"
-fn main() {}
-(1)
-(2)
-(3)
-(4)
-(5)
-ret 0
-",
-    "fn main(){}let x:i32 = 0",
-    r"
-fn no_return_type_but_has_return() { ret 0 }
-fn no_return_statement() 
-  -> i32 {
-}
-fn voidfunc(){}
-voidfunc() # ok
-# fn f() {}  fn f() -> i32 {}
-let x: WrongType = 0
-call_f()
-ret variable
-voidfunc() + 1 # ??
-(2)
-voidfunc(1, 2, 3)
-",
-    r"
-fn f(){}
-  f()
-  +
-  1
-9",
-    // basic
-    r"
-fn main() -> i32 {
-  ret 1+1
-}
-",
-    // no main
-    r"
-ret 2
-",
-    r"
-fn f1() -> i32 {
-  ret 1
-}
-fn f2() -> i32 {
-  ret 2
-}
-fn main() -> i32 {
-  ret f1() + f2() * 3
-}
-",
-    // nest
-    r"
-fn f0() {
-    fn f2() {
-        fn f5() {
-            f0() # ok
-            # f8() <- NG
-        }
-        fn f6() -> i32 { ret 4 }
-    }
-}
-fn f1() {
-    fn f3() {
-        fn f7() {
-            f8() # ok
-            fn f8() -> i32 { ret 8 }
-        }
-    }
-    fn f4() -> i32 { ret 4 }
-}
-",
-    // variables
-    r"
-fn variables() -> i32 {
-  let a: i32 = 10
-  let b: i32 = 5
-  let c: i64 = 600
-  let d: i64 = 10000
-  let a: i64 = 500 # shadowing
-  ret a+b+c+d
-}
-",
-    // some expressions
-    r"
-fn test() {
-  -1
-  2
-  (+3)
-  4+11   3-2
-  10+1 * 6/3
-  let value: i32 = 0
-}
-fn expr() {
-  ret
-    10+20
-    -
-    # comment is treated as spaces
-    1+3*6/(1+1) - 2
-}
-#*
-fn inside_comment() {
-}
-*#
-",
-    // function
-    r"
-fn add_2a_b(a: i32, b: i32) -> i64 {
-  let a: i32 = a*2 # shadowing
-  ret a+b
-}
-fn variables() -> i32 {
-  let a: i32 = 10
-  let b: i32 = 5
-  ret a + b*2 + add_2a_b(5, b)
-}
-fn main() -> i32 {
-  ret variables()
-}
-    ",
-    // setter
-    r"
-fn main() -> i32 {
-  let x: i32 | double = 10 # x is 10; initialize and assignment is different
-  x.set(x) # todo, x will become 20
-  let one: i32 | set_one = 0
-  one.set() # one will become 1
-  ret x+one # 20 + 1
-}
-fn double(in: i32) -> i32 {
-  ret in*2
-}
-fn set_one() -> i32 {
-  ret 1
-}
-",
-    r"
-# return n!
-fn factorial(n: i32) -> i32 {
-  if n == 0 { ret 1 }
-  31
-  ret n * factorial(n-1)
-}
-# function named `main` is entrypoint
-fn main() -> i32 {
-  ret factorial(4)
-}
-",
-  ]
-  .iter()
-  .enumerate()
-  {
-    println!("----------------------------------------------");
-    // basically above examples start with '\n'
-    // so print `input{}`, not `input\n{}`
-    println!("=== input : ```{}```", code);
-    println!("=== compile result");
-    let mut code = backend::source_code::SourceCode::new(code);
-    let program = match backend::compile(&mut code) {
-      Ok(program) => {
-        if code.remaining_len() != 0 {
-          println!("[error] code is not consumed entierly");
-          println!("        remaining=```{}```", code.remaining_code());
-          continue;
-        }
-        program
+  let mut built_options = BuildOptions::new();
+  let mut args = env::args().into_iter();
+  let command_name = args.next().unwrap();
+  loop {
+    let Some(arg) = args.next() else { break };
+    match arg.as_str() {
+      "-h" | "--help" => {
+        println!(
+          "Usage: {} [-h|--help] [--verbose] [-o|--out] <file>",
+          command_name
+        );
+        process::exit(0);
       }
-      Err(error) => {
-        println!("{}", error);
-        continue;
+      "-v" | "--version" => {
+        println!("Settlang v{}", env!("CARGO_PKG_VERSION"));
+        process::exit(0);
       }
-    };
+      "--verbose" => built_options.verbose = true,
+      "-o" | "--out" => {
+        if built_options.output_path.is_some() {
+          println!("[error] '-o'/'--out' is specified multiple times");
+          process::exit(1);
+        }
+        built_options.output_path = Some(args.next().unwrap_or_else(|| {
+          println!("[error] missing output path after '{}'", arg);
+          process::exit(1);
+        }));
+      }
+      _ => {
+        if built_options.input_path.is_some() {
+          println!("[error] please specify only one input file");
+          process::exit(1);
+        }
+        built_options.input_path = Some(arg);
+      }
+    }
+  }
+
+  let Some(input_path) = built_options.input_path else {
+    println!("[error] no input file");
+    process::exit(1);
+  };
+
+  let code_string = fs::read_to_string(input_path).unwrap_or_else(|error| {
+    println!("[error] {}", error);
+    process::exit(1);
+  });
+  let mut code = backend::source_code::SourceCode::new(&code_string);
+  let program = backend::compile(&mut code).unwrap_or_else(|error| {
+    println!("{}", error);
+    process::exit(1);
+  });
+  if built_options.verbose {
+    println!("=== compile result\nremaining_len={}", code.remaining_len());
+  }
+  if code.remaining_len() != 0 {
+    println!("[error] code is not consumed entierly");
+    if built_options.verbose {
+      println!("        remaining=```{}```", code.remaining_code());
+    }
+    process::exit(1);
+  }
+
+  if built_options.verbose {
     println!("Succeeded to compile || {:#?}", program);
-    match backend::builder::wasm::build(program) {
-      Ok(wasm) => {
-        let filename = format!("out/{}.wasm", i);
-        match std::fs::File::create(&filename).and_then(|mut file| file.write_all(&wasm)) {
-          Ok(_) => println!("Saved as '{}'", filename),
-          Err(e) => println!("Failed to save WASM file : {:?}", e),
+  }
+  match backend::builder::wasm::build(program) {
+    Ok(wasm) => {
+      let filename = built_options.output_path.unwrap_or("out.wasm".to_string());
+      match fs::File::create(&filename).and_then(|mut file| file.write_all(&wasm)) {
+        Ok(_) => {
+          if built_options.verbose {
+            println!("Saved as '{}'", filename)
+          }
         }
+        Err(e) => println!("[error] failed to save WASM file : {}", e),
       }
-      Err(error) => println!("Failed to build WASM file : {}", error),
     }
+    Err(error) => println!("[error] failed to build WASM file : {}", error),
   }
 }
